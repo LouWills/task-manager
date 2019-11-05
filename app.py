4010ad91e11a812b3b161a33ccbc045029c52aaf
Ubuntu <ubuntu@ip-172-31-82-1.ec2.internal>
2019-11-05 17:04:26 +0000

    Minor Changes

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = 'mongodb+srv://root_24:rOoTuSeR16@myfirstcluster-bfxp2.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
-
 
 @app.route('/')
 @app.route('/get_tasks')
